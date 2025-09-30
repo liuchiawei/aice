94b32eb9787b6b305aca0ba4ee1e4124e20a6eee
LIU CHIAWEI <doublecheap@gmail.com>
2025-09-30 16:50:28 +0900

    add content.json

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -1,7 +1,19 @@
-export default function Nav() {
+import Link from "next/link"
+import { NavigationMenu, NavigationMenuList, NavigationMenuItem, NavigationMenuLink } from "@/components/ui/navigation-menu"
+import content from "@/data/content.json"
+
+export function Nav() {
   return (
-    <nav>
-      
-    </nav>
-  );
+    <NavigationMenu>
+      <NavigationMenuList>
+        {content.nav.map((item) => (
+          <NavigationMenuItem key={item.name}>
+            <NavigationMenuLink asChild>
+              <Link href={item.href}>{item.name}</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        ))}
+      </NavigationMenuList>
+    </NavigationMenu>
+  )
 }
